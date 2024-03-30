8ade75bc6e745d8bf077af4974855b98bd6c9e20
quangminhhh <9a1minhcl@gmail.com>
2024-03-30 17:02:11 +0700

    30

--- src/server.js
+++ src/server.js
@@ -6,6 +6,30 @@ const webRoutes = require('./routers/web'); // Web routes
 const app = express(); // App express
 const port = process.env.PORT || 8888; // Port
 const hostname = process.env.HOST_NAME; // Hostname
+// Get the client
+const mysql = require('mysql2')
+
+// test connection
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307, // default: 3306
+  user: 'root',
+  password: '123456789',
+  database: 'quangminh_mysql',
+});
+
+// A simple SELECT query
+connection.query(
+    'SELECT * FROM Users u',
+function (err, results, fields)
+{
+          console.log('>>>check err:', err);
+          console.log('>>>check results: ', results);
+          console.log('>>>check fields: ', fields);
+});
+
+
 
 // config template engine
 configViewEngine(app);
