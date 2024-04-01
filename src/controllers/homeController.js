ea8dcb334fcf0ee14b3efa93212e473d46832e6b
quangminhhh <9a1minhcl@gmail.com>
2024-04-01 21:05:48 +0700

    #35

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -5,17 +5,7 @@ const getHomepage = (req, res) =>
     // process data
     // call model
     // A simple SELECT query
-    let users = [];
-    connection.query(
-        'SELECT * FROM Users u',
-    function (err, results)
-    {
-        users = results;
-        console.log('>>>check err:', err);
-        console.log('>>>results homepage: ', results);
-        // console.log('>>>check users:', users);
-        res.send(JSON.stringify(users));
-    });
+    return res.render('home.ejs')
 }
 
 const getSecondpage = (req, res) =>
