d2b385076853200140a10fdc2b1d1368aed0babc
quangminhhh <9a1minhcl@gmail.com>
2024-04-06 21:34:48 +0700

    #38

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -3,11 +3,12 @@ const express = require('express'); // conmmon js module
 
 const configViewEngine = (app) => {
     // config template engine
-    app.set('views', path.join('./src', 'views')) 
+    app.set('views', path.join('./src', 'views'))
     app.set('view engine', 'ejs')
 
     // config static file
     app.use(express.static(path.join('./src', 'public')))
+
 }
 
 module.exports = configViewEngine; // Export module
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,26 +1,33 @@
 const connection = require('../config/database');
 
-const getHomepage = (req, res) => 
-{
+const getHomepage = (req, res) => {
     // process data
     // call model
     // A simple SELECT query
     return res.render('home.ejs')
 }
 
-const getSecondpage = (req, res) =>
-{
+const getSecondpage = (req, res) => {
     res.render('sample.ejs');
 }
 
-const postCreateUser = (req, res) =>
-{
-    console.log('>>>check req.body:', req.body);
-    // res.send("Create user successfully");
-    res.render('create-user.ejs');
+const postCreateUser = (req, res) => {
+    let email = req.body.email;
+    let name = req.body.myname;
+    let city = req.body.city;
+    console.log('>>>check req.body:','\nemail:', email,'\nname:', name, '\ncity:', city);
+
+    connection.query(
+        `INSERT INTO Users (email, name, city) 
+        VALUES (?, ?, ?)`,
+        [email, name, city],
+        function (err, results) {
+          console.log(results);
+        }
+      );
 }
 
-module.exports = 
+module.exports =
 {
     getHomepage,
     getSecondpage,
